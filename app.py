84018fd6778d8e7ab4956a1e07471eb6eb631d86
TechOnTips <65756827+TechOnTips@users.noreply.github.com>
2020-07-06 18:51:47 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,46 @@
+import tweepy
+from time import sleep
+import os
+from os import environ
+
+consumer_key = environ['consumer_key']
+consumer_secret = environ['consumer_secret']
+access_token = environ['access_token']
+access_token_secret = environ['access_token_secret']
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+LIKE = True
+
+FOLLOW = False
+
+print("Twitter bot which retweets, like tweets and follow users")
+print("Bot Settings")
+print("Like Tweets :", LIKE)
+print("Follow users :", FOLLOW)
+
+for tweet in tweepy.Cursor(api.search, q=('#feeskumkaro OR #AKGEC OR #akgec -filter:retweets'), lang='en').items():
+    try:
+        print('\nTweet by: @' + tweet.user.screen_name)
+
+        tweet.retweet()
+        print('Retweeted the tweet')
+
+        if LIKE:
+            tweet.favorite()
+            print('Favorited the tweet')
+
+        if FOLLOW:
+            if not tweet.user.following:
+                tweet.user.follow()
+                print('Followed the user')
+
+        sleep(300)
+
+    except tweepy.TweepError as e:
+        print(e.reason)
+
+    except StopIteration:
+        break
